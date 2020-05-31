b73c59d16e1970e5f22e037cd93dde583149ac4e
J.E. R <je-r@users.noreply.github.com>
2020-06-01 00:29:37 +0200

    dev

--- play.js-React.js-with-TSX-TypeScript/README.md
+++ play.js-React.js-with-TSX-TypeScript/README.md
@@ -1,1 +1,6 @@
 play.js has templates only for *.jsx but not *.tsx, this sub-project tries to solve this problem
+
+
+maybe fix: 
+
+https://github.com/babel/babel/issues/8711
--- play.js-React.js-with-TSX-TypeScript/webpack.config.js
+++ play.js-React.js-with-TSX-TypeScript/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
   mode: 'development',
   entry: {
     app: [
